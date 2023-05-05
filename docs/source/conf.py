ef5bb5c70a23759a31ba30aaaed074f09b3cc130
wangkui0508 <wangkui0508@hotmail.com>
2023-05-05 12:35:16 +0800

    remove extensions to try python2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,14 +11,14 @@ version = '0.1.0'
 
 # -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel',
-]
+#extensions = [
+#    'sphinx.ext.duration',
+#    'sphinx.ext.doctest',
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.autosummary',
+#    'sphinx.ext.intersphinx',
+#    'sphinx.ext.autosectionlabel',
+#]
 autosectionlabel_prefix_document = True
 
 intersphinx_mapping = {
